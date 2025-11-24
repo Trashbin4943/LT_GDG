8b9288610ad2b4c0d7e6ccf356c18df2b8bd9131
Trashbin4943 <anthonyhwan415@gmail.com>
2025-11-25 00:40:41 +0900

    refactor: 런타임 환경 음성분석 대기시간 600초 상향

--- Dockerfile
+++ Dockerfile
@@ -28,4 +28,4 @@ RUN pip install --no-cache-dir -r requirements.txt
 
 COPY . .
 
-CMD ["gunicorn", "linguaproject.wsgi:application", "--bind", "0.0.0.0:8080", "--timeout", "120"]
+CMD ["gunicorn", "linguaproject.wsgi:application", "--bind", "0.0.0.0:8080", "--timeout", "600"]
--- linguaproject/settings.py
+++ linguaproject/settings.py
@@ -9,21 +9,14 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 import environ
-import os
-
-import os
-HF_TOKEN = os.getenv("HF_TOKEN")
+from dotenv import load_dotenv
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-env = environ.Env(
-    DEBUG=(bool, False)
-)
-
-environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+load_dotenv(os.path.join(BASE_DIR, '.env'))
+env = environ.Env()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
@@ -110,18 +103,25 @@ WSGI_APPLICATION = 'linguaproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': env('DB_ENGINE'),
+#         'NAME': env('DB_NAME'),
+#         'USER': env('DB_USER'),
+#         'PASSWORD': env('DB_PASSWORD'),
+#         'HOST': env('DB_HOST'),
+#         'PORT': env('DB_PORT'),
+#         'OPTIONS': {
+#             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+#             'charset': 'utf8mb4',
+#         },
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': os.environ.get('DB_ENGINE'),  # DB 엔진
-        'NAME': os.environ.get('DB_NAME', 'django_db'),      # DB 이름
-        'USER': os.environ.get('DB_USER', 'root'),           # DB 사용자명
-        'PASSWORD': os.environ.get('DB_PASSWORD', ''),       # DB 비밀번호
-        'HOST': os.environ.get('DB_HOST', 'localhost'),      # MySQL 호스트 주소 (IP 또는 도메인)
-        'PORT': os.environ.get('DB_PORT', '3306'),           # MySQL 포트 (보통 3306)
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-            'charset': 'utf8mb4',
-        },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
