371f7e85a096ac1252ce9248266bc2a631e08995
Trashbin4943 <anthonyhwan415@gmail.com>
2025-11-24 19:20:27 +0900

    Solve: merge conflicts solve

--- logical_analysis/models.py
+++ logical_analysis/models.py
@@ -8,23 +8,15 @@
 #         related_name='logical_analysis'
 #     )
 
-<<<<<<< HEAD
 #     speech_speed = models.FloatField(null=True, blank=True)
 #     pause_duration = models.FloatField(null=True, blank=True)
 #     is_overlap = models.BooleanField(default=False)
-=======
-    # 기존 음성/행동 분석 메트릭
-    speech_speed = models.FloatField(null=True, blank=True)
-    pause_duration = models.FloatField(null=True, blank=True)
-    is_overlap = models.BooleanField(default=False)
->>>>>>> 683ffcf40d3180e99bb671bf791765358620a5be
 
 #     profanity_score = models.FloatField(null=True, blank=True)
 #     threat_score = models.FloatField(null=True, blank=True)
 #     insistence_score = models.FloatField(null=True, blank=True)
 #     intent_label = models.CharField(max_length=100, null=True, blank=True)
 
-<<<<<<< HEAD
 #     manual_compliance_score = models.FloatField(null=True, blank=True)
 #     empathy_score = models.FloatField(null=True, blank=True)
 
@@ -33,22 +25,3 @@
 #     class Meta:
 #         db_table = 'logical_results'
 #         ordering = ['segment__start_time']
-=======
-    manual_compliance_score = models.FloatField(null=True, blank=True)
-    empathy_score = models.FloatField(null=True, blank=True)
-    context_appropriateness = models.FloatField(null=True, blank=True)
-
-    # ClassificationResult에서 사용되던 필드(호환용)
-    label = models.CharField(max_length=100, null=True, blank=True)
-    label_type = models.CharField(max_length=50, null=True, blank=True)
-    confidence = models.FloatField(null=True, blank=True)
-    probabilities = models.JSONField(null=True, blank=True)
-    action = models.CharField(max_length=50, null=True, blank=True)
-    alert_level = models.CharField(max_length=20, null=True, blank=True)
-    timestamp = models.DateTimeField(null=True, blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    class Meta:
-        db_table = 'logical_results'
-        ordering = ['segment__start_time']
->>>>>>> 683ffcf40d3180e99bb671bf791765358620a5be
